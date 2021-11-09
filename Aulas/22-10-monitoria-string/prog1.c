66eb8a8791c4925571b7ca3c1f601440d1d2aac0
r0b14 <rtth141200@gmail.com>
2021-11-09 18:31:57 -0300

    arquivos atualizados - maquina linux

---
+++ Aulas/22-10-monitoria-string/prog1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#define tam_vet 10;
+
+int main() {
+
+   // STOI - String to Int [transformando uma string de numero em int]
+   char numeroS[] = "1412560";
+   int numeroInt = 0, str_size = strlen(numeroS) - 1, index = 0;
+   int charNumerico = 0; // Variavel que vai armazenar a mudanca de string p/ int.
+
+   while(numeroS[index] != '\0') {
+      charNumerico = ((int)numeroS[index]) - 48; //'1' = 49 - 48 = 1 -> ASCII
+      printf("Char convertido %d\n", charNumerico);
+
+      charNumerico *= pow(10, str_size - index); // 1 * 10^(8-1) - 1(index) -> 1 * 10^6 = 1000000
+      printf("%d valor convertido = %d\n", index+1 ,charNumerico);
+
+      numeroInt += charNumerico;
+      ++index;
+   }
+
+   printf("%d\n", numeroInt);
+
+   return 0;
+
+}
