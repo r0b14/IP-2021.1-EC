1a58a75477dd2a12a8f975ddc546208f2791bcf5
r0b14 <rtth141200@gmail.com>
2021-11-23 20:54:38 -0300

    Criado arquivo da questao 2

--- Atividades/Atividade06-Classroom/processando/01.c
+++ Atividades/Atividade06-Classroom/processando/01.c
@@ -2,7 +2,20 @@
 #include <stdlib.h>
 
 int main() {
+   char c='a';
+   char *pc;
+   pc = &c;
 
+   // A) Endereço em que C esta localizado e o Valor de C
+   printf("Endereco c - %p\n", &c);
+   printf("Valor c - %d\n", c);
+
+   // B e C) Valor de PC, endereço de pc e o valor guardado.
+   printf("Valor de pc - %d\n", *pc);
+   printf("%d\n", pc);
+   printf("Endereco pc - %p\n", pc);
+
+   //
 
    return 0;
 }
